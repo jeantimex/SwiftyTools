f24106fe45391ccc0e0d4043da6c6733a4e0ce44
jeantimex <jean.timex@gmail.com>
2019-02-22 11:55:41 -0800

    Setup targets.

---
+++ SwiftyTools/SwiftyTools.h
@@ -0,0 +1,18 @@
+//
+//  SwiftyTools.h
+//  SwiftyTools
+//
+//  Created by Yong Su on 2/22/19.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyTools.
+FOUNDATION_EXPORT double SwiftyToolsVersionNumber;
+
+//! Project version string for SwiftyTools.
+FOUNDATION_EXPORT const unsigned char SwiftyToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyTools/PublicHeader.h>
+
+
